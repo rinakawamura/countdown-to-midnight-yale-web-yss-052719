3cabb7c149a2f5809cb13b68bca2831b84e9d234
Rina Kawamura <rina.kawamura@yale.edu>
2019-04-28 20:51:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(int)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
